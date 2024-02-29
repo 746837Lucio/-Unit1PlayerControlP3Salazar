47375641dabbb455dd028d0f2db8414cf3e435f2
746837Lucio <luciosalazar0503@gmail.com>
2024-02-29 11:39:39 -0800

    I added moving cars and alot of obstacles
    I added movinjg cARSA ALO9T OF OBSTACLES

--- SalazarUnit1PlayerControlP3/Assets/Scripts/PlayerController.cs
+++ SalazarUnit1PlayerControlP3/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,9 +12,21 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float speed = 20;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
     void Update()
     {
         // Move the vehicle foward
-        transform.Translate(0, 0, 1);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vbertical input
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, horizontalInput);
+        
     }
 }
