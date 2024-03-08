d645879c4c2f2fd1c747a7fa8ba8c4d31f68ee5b
746837Lucio <luciosalazar0503@gmail.com>
2024-03-08 11:19:37 -0800

    Made a plane game with 2 sciprts
    made a pnae game and added 2 sciprtsd

--- SalazarUnit1PlayerControlP3/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ SalazarUnit1PlayerControlP3/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -6,11 +6,10 @@ public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
     private Vector3 offset;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - plane.transform.position;
     }
 
     // Update is called once per frame
